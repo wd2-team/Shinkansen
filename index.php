593c5b5b7221d2e61f642e0983fbf59b803ef4d5
Jewelry <supirs102@gmail.com>
2022-08-25 16:20:45 +0900

    メインページ作成中

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
   	src="https://code.jquery.com/jquery-2.2.4.js"
   	integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
   	crossorigin="anonymous"></script>
+  	<link rel="stylesheet" type="text/css" href="css/modal.css">
 	<?php $Path = "./"; include(dirname(__FILE__).'/functions.php'); ?>
   	<?php if( is_mobile()) : ?>
     <link rel="stylesheet" type="text/css" href="css/mobile.css">
@@ -25,7 +26,45 @@
 <body>
 <div id="wrapper">
 	<header>
+		<div id="headchouchin1" class="blinkred">
+			<div class="headchouchin">
+				<img src="images/chouchin_kouhaku.png">
+				<img src="images/chouchin_kouhaku.png">
+				<img src="images/chouchin_kouhaku.png">
+			</div>
+			<p>9.23</p>
+		</div>
+		<div id="headchouchin2" class="blinkwhite">
+			<div class="headchouchin">
+				<img src="images/chouchin_kouhaku.png">
+				<img src="images/chouchin_kouhaku.png">
+				<img src="images/chouchin_kouhaku.png">
+			</div>
+			<p>9.24</p>
+		</div>
+		<div id="headchouchin3" class="blinkred">
+			<div class="headchouchin">
+				<img src="images/chouchin_kouhaku.png">
+				<img src="images/chouchin_kouhaku.png">
+				<img src="images/chouchin_kouhaku.png">
+			</div>
+			<p>9.25</p>
+		</div>
 	</header>
+	<div id="headlink">
+		<label for="modal-trigger01">モーダルが開きます</label>
+		<div class="modal">
+		  <input id="modal-trigger01" class="checkbox" type="checkbox">
+		  <div class="modal-overlay">
+		    <label for="modal-trigger01" class="o-close">&#10006;</label>
+		    <div class="modal-wrap a-center">　/*a-centerがウィンドウを中心に設置*/
+		      <label for="modal-trigger01" class="close">&#10006;</label>
+		      <h2>モーダルのタイトル</h2>
+		      <p>テキストテキストテキストテキストテキスト</p>
+		    </div>
+		  </div>
+		</div>
+	</div>
 	
 	<footer>
 	</footer>
