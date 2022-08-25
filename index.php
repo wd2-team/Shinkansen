de9a48f5daca1dc17a835942787971a207872b04
SSM <cvwwvc102@gmail.com>
2022-08-25 15:30:19 +0900

    リダイレクト修正　ファイル名変更

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<meta property="og:type" content="website">
 	<meta property="og:title" content="西九州新幹線開通元年">
 	<meta property="og:description" content="西九州新幹線開通元年　つながりましておめでとうございます">
+	<!-- <meta http-equiv="refresh" content="5;URL=index.php"> -->
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
 	<script
   	src="https://code.jquery.com/jquery-2.2.4.js"
@@ -14,6 +15,7 @@
   	crossorigin="anonymous"></script>
 	<?php $Path = "./"; include(dirname(__FILE__).'/functions.php'); ?>
   	<?php if( is_mobile()) : ?>
+		<meta http-equiv="refresh" content="4.5;URL=index.php">
     <link rel="stylesheet" type="text/css" href="css/mobile.css">
     <?php elseif( is_tablet()) : ?>
     <link rel="stylesheet" type="text/css" href="css/tablet.css">
@@ -26,7 +28,34 @@
 <div id="wrapper">
 	<header>
 	</header>
-	
+	<div id="topwrapper">
+		<div id="topimages">
+			<div id="topleft">
+				<img class="delay-time07 fadeUp" src="images/o.png">
+				<img class="delay-time08 fadeUp" src="images/me.png">
+				<img class="delay-time09 fadeUp" src="images/de.png">
+				<img class="delay-time10 fadeUp" src="images/to.png">
+				<img class="delay-time11 fadeUp" src="images/u.png">
+				<img class="delay-time12 fadeUp" src="images/go.png">
+				<img class="delay-time13 fadeUp" src="images/za.png">
+				<img class="delay-time14 fadeUp" src="images/i.png">
+				<img class="delay-time15 fadeUp" src="images/ma.png">
+				<img class="delay-time16 fadeUp" src="images/su.png">
+			</div>
+			<div id="topright">
+				<img class="fadeUp" src="images/tsu.png">
+				<img class="delay-time01 fadeUp" src="images/na.png">
+				<img class="delay-time02 fadeUp" src="images/ga.png">
+				<img class="delay-time03 fadeUp" src="images/ri.png">
+				<img class="delay-time04 fadeUp" src="images/ma.png">
+				<img class="delay-time05 fadeUp" src="images/shi.png">
+				<img class="delay-time06 fadeUp" src="images/te.png">
+			</div>
+		</div>
+		<div id="topbottom">
+			<img class="slideUp" src="images/train-1.png">
+		</div>
+	</div><!-- #topwrapper -->
 	<footer>
 	</footer>
 </div><!-- #wrapper -->
