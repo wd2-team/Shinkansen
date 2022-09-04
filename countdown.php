26c2b351c0e8d783cae0bb4f109c0ddcb94e9e7c
Jewelry <supirs102@gmail.com>
2022-09-04 21:02:16 +0900

    メインページ、カウントダウンページ作成中

--- countdown.php
+++ countdown.php
@@ -7,7 +7,6 @@
 	<meta property="og:type" content="website">
 	<meta property="og:title" content="西九州新幹線開通元年">
 	<meta property="og:description" content="西九州新幹線開通元年　つながりましておめでとうございます">
-	<!-- <meta http-equiv="refresh" content="5;URL=index.php"> -->
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
 	<script
   	src="https://code.jquery.com/jquery-2.2.4.js"
@@ -23,9 +22,9 @@
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f%7C%7Ca&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
 </script>
+  	<link rel="stylesheet" type="text/css" href="css/modal.css">
 	<?php $Path = "./"; include(dirname(__FILE__).'/functions.php'); ?>
   	<?php if( is_mobile()) : ?>
-	<meta http-equiv="refresh" content="5;URL=main.php">
     <link rel="stylesheet" type="text/css" href="css/mobile.css">
     <?php elseif( is_tablet()) : ?>
     <link rel="stylesheet" type="text/css" href="css/tablet.css">
@@ -35,36 +34,14 @@
 	<title>西九州新幹線開通元年　つながりましておめでとうございます</title>
 </head>
 <body>
-<div id="indexwrapper">
-	<div id="topwrapper">
-		<div id="topimages">
-			<div id="topleft">
-				<img class="delay-time07 fadeUp" src="images/o.png">
-				<img class="delay-time08 fadeUp" src="images/me.png">
-				<img class="delay-time09 fadeUp" src="images/de.png">
-				<img class="delay-time10 fadeUp" src="images/to.png">
-				<img class="delay-time11 fadeUp" src="images/u.png">
-				<img class="delay-time12 fadeUp" src="images/go.png">
-				<img class="delay-time13 fadeUp" src="images/za.png">
-				<img class="delay-time14 fadeUp" src="images/i.png">
-				<img class="delay-time15 fadeUp" src="images/ma.png">
-				<img class="delay-time16 fadeUp" src="images/su.png">
-			</div>
-			<div id="topright">
-				<img class="fadeUp" src="images/tsu.png">
-				<img class="delay-time01 fadeUp" src="images/na.png">
-				<img class="delay-time02 fadeUp" src="images/ga.png">
-				<img class="delay-time03 fadeUp" src="images/ri.png">
-				<img class="delay-time04 fadeUp" src="images/ma.png">
-				<img class="delay-time05 fadeUp" src="images/shi.png">
-				<img class="delay-time06 fadeUp" src="images/te.png">
-			</div>
-		</div>
-		<div id="topbottom">
-			<img class="slideUp" src="images/train-1.png">
-		</div>
-	</div><!-- #topwrapper -->
-</div><!-- #indexwrapper -->
+<div id="countdownwrapper" class="page">
+	<img src="images/kaitsu-gannen.svg">
+	<div id=count class="container">
+		<p>西九州新幹線開通まで</p>
+		<p>あと<span class="today"></span>日</p>
+		<p>主催：諫早市新幹線開業記念事業実行委員会</p>
+	</div>
+</div>
 <script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
