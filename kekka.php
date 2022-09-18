ec1caa41af6af3db8c58d49550cd3a1bfe534d3a
Jewelry <supirs102@gmail.com>
2022-09-18 17:07:04 +0900

    結果ページ作成中

--- kekka.php
+++ kekka.php
@@ -12,6 +12,16 @@
   	src="https://code.jquery.com/jquery-2.2.4.js"
   	integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
   	crossorigin="anonymous"></script>
+    <script>
+  (function(d) {
+    var config = {
+      kitId: 'qbw3zuc',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f%7C%7Ca&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@900&display=swap" rel="stylesheet">
@@ -38,35 +48,37 @@
 </head>
 <body>
 <div id="kekkawrapper" class="page">
-  <img src="images/start.png">
-  <p>開&emsp;通&emsp;祈&emsp;願<br><span>お&emsp;み&emsp;く&emsp;じ</span></p>
-  <div id="kekkacontent">
-    <div id="kekkabox01">
-      <div id="kekkabox01-01">
-        <img class="kujikekka" src="">
-      </div>
-      <div id="kekkabox01-02">
-        万事順調。諫早にはお米を食べて育つ<br>美味しい「諫美豚」がいるよ
-      </div>
-    </div>
-    <div id="kekkabox02">
-      <div id="kekkabox02-01">ラッキースポット</div>
-      <div id="kekkabox02-02">
-        <div>
-          <p>ハートのイチョウ</p>
-          <p>飯盛ふれあい会館には<br>ハートの形をした銀杏の木があり<br>秋には黄色のハートが見れるよ</p>
-          <i class="fa-solid fa-location-dot"></i>
+  <img src="images/omikuji_kumo.svg">
+  <p>諫&emsp;早&emsp;市&emsp;限&emsp;定<br><span class="omikujifont">お&emsp;み&emsp;く&emsp;じ</span></p>
+  <div id="kekkaback">
+    <div id="kekkacontent">
+      <div id="kekkabox01">
+        <div id="kekkabox01-01">
+          <img class="kujikekka" src="">
+        </div>
+        <div id="kekkabox01-02">
+          万事順調。諫早にはお米を食べて育つ<br>美味しい「諫美豚」がいるよ
         </div>
       </div>
+      <div id="kekkabox02">
+        <div id="kekkabox02-01">ラッキースポット</div>
+        <div id="kekkabox02-02">
+          <div>
+            <p>ハートのイチョウ</p>
+            <p>飯盛ふれあい会館には<br>ハートの形をした銀杏の木があり<br>秋には黄色のハートが見れるよ</p>
+            <i class="fa-solid fa-location-dot"></i>
+          </div>
+        </div>
+      </div>
+      <div id="kekkabox03">
+        <p><button onclick="drawKuji()">もう一度挑戦する</button></p>
+      </div>
+      <div id="kekkabox04">
+        <img src="images/kaitsu_type_b.svg">
+      </div>
+      <a href="#">気運を高める<br>開通元年イベントはこちら↘</a>
+      <img src="images/shinkansen_kamome_moji.svg">
     </div>
-    <div id="kekkabox03">
-      <p><button onclick="drawKuji()">もう一度挑戦する</button></p>
-    </div>
-    <div id="kekkabox04">
-      <img src="images/kaitsu_type_b.svg">
-    </div>
-    <a href="#">気運を高める<br>開通元年イベントはこちら↘</a>
-    <img src="images/shinkansen_kamome_moji.svg">
   </div>
 </div>
 <script type="text/javascript" src="js/main.js"></script>
--- main.php
+++ main.php
@@ -22,16 +22,6 @@
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f%7C%7Ca&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
 </script>
-<!-- <script>
-  (function(d) {
-    var config = {
-      kitId: 'guc3bwj',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f%7C%7Ca&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-</script> -->
   	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@900&display=swap" rel="stylesheet">
