9c22b45f57bb31364ece4470a93db0457aa1456e
Jewelry <supirs102@gmail.com>
2022-09-18 19:24:02 +0900

    結果ページ作成中

--- kekka.php
+++ kekka.php
@@ -54,30 +54,38 @@
     <div id="kekkacontent">
       <div id="kekkabox01">
         <div id="kekkabox01-01">
-          <img class="kujikekka" src="">
+          <div id="kekkabox01-01top">
+            <img id="kekkabox01-01unagihidari" src="images/unagi_hidari.svg">
+            <span id="kekkabox01-01title">運&emsp;&emsp;勢</span>
+            <img id="kekkabox01-01unagimigi" src="images/unagi_migi.svg">
+          </div>
+          <div id="kekkabox01-01content" class="omikujifont">
+            トントン拍子に<br>物事がすすむ
+          </div>
+          <!-- <img class="kujikekka" src=""> -->
         </div>
-        <div id="kekkabox01-02">
+        <div id="kekkabox01-02" class="omikujifont">
           万事順調。諫早にはお米を食べて育つ<br>美味しい「諫美豚」がいるよ
         </div>
       </div>
       <div id="kekkabox02">
-        <div id="kekkabox02-01">ラッキースポット</div>
-        <div id="kekkabox02-02">
-          <div>
-            <p>ハートのイチョウ</p>
-            <p>飯盛ふれあい会館には<br>ハートの形をした銀杏の木があり<br>秋には黄色のハートが見れるよ</p>
-            <i class="fa-solid fa-location-dot"></i>
-          </div>
+        <div id="kekkabox02-01" class="omikujifont">ラッキースポット</div>
+        <div id="kekkabox02-02" class="omikujifont">
+          <p>ハートのイチョウ</p>
+          <p>飯盛ふれあい会館には<br>ハートの形をした銀杏の木があり<br>秋には黄色のハートが見れるよ</p>
         </div>
       </div>
-      <div id="kekkabox03">
+      <div id="kekkabox03" class="omikujifont">
         <p><button onclick="drawKuji()">もう一度挑戦する</button></p>
       </div>
       <div id="kekkabox04">
         <img src="images/kaitsu_type_b.svg">
+        <a href="#"><span>開通元年イベント</span><br>特設サイトはこちら</a>
+        <p>9月23日･24日･25日<br>諫早市内の各会場で開催</p>
+        <a href="#">おみくじアイディア募集中</a>
+        <p>主催：諫早市新幹線開業記念事業実行委員会</p>
+        <img src="images/shinkansen_kamome_moji.svg">
       </div>
-      <a href="#">気運を高める<br>開通元年イベントはこちら↘</a>
-      <img src="images/shinkansen_kamome_moji.svg">
     </div>
   </div>
 </div>
