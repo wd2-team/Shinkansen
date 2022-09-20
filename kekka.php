97fbdebfefac87419e6397886a398446fd28e161
Jewelry <supirs102@gmail.com>
2022-09-20 17:03:00 +0900

    おみくじ作成中

--- kekka.php
+++ kekka.php
@@ -54,64 +54,101 @@
   <div id="kekkaback">
     <div id="kekkacontent">
       <?php
-      function get_luck_data() {
-        $url = 'https://script.googleusercontent.com/macros/echo?user_content_key=D71An70cQR7IdEEwyVUdM7AprFbQOxbk37_DIl9GQJfXvY28Ek2_SO6dE08cbEkOfN47m4VzTlN0FcqOup7Nlqdn-9gvHIBzm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIXzT647ONc3aWmr4tbRmd2sLLk5gn7T-Uzd3xfo-HZd2UFjJ02hUWncQtNUlYwVhmeHF1aVl2Vt_i4YEl6wa6N3sE1xyZpz6dz9Jw9Md8uu&lib=MGCGSwU88mgjR4EiuwwnVWK_uYdcHmPPI';
-        if ( file_get_contents( $url ) ) {
-          $data = mb_convert_encoding( file_get_contents( $url ), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
-          $json_decode = json_decode($data, true);
-          return $json_decode;
-        }
-      }
-      function get_item_data() {
-        $url = 'https://script.googleusercontent.com/macros/echo?user_content_key=5ISYrODQUHB60gPs-ZkMWNgUEe-r6MygDcTdoNtlT92q-jSH3JaiVR9XrGZ_sMnb4tnEggmJ-z5d2ml3Nl7vuEcGyAm49CNXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnAUP0sBXi52xIy_p6v-6Lz4pj5WAk1clfHKWY_g6Usl8B3I9LSX7b9wbimy255tdC47ZUUuF93hWMwwv3CkrwcS7KsGIIxohiw&lib=MURsQyvxp9xRfkImfm-2mbhy_24_rZK5a';
-        if ( file_get_contents( $url ) ) {
-          $data = mb_convert_encoding( file_get_contents( $url ), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
-          $json_decode = json_decode($data, true);
-          return $json_decode;
-        }
-      }
-      function get_spot_data() {
-        $url = 'https://script.googleusercontent.com/macros/echo?user_content_key=cDVjNLGlSB51MLhFkDr1N38k-1rToEJP400Vd-WR8MOD9WEp_8gTEJSKaQET1h3nNu8BMNDD315d2ml3Nl7vuKXZzMK4L4R1m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnGrd7XCOjJun8jMlrgZtXTEN5zJblhTiqjkYYVAok-k5cfhfMQLJ_aonOidiBM2f90Es2jQ7FfD6RXV4URG5OlcRBhJtVIztNA&lib=MRM0HZ0153TD_AdZUnIivUxy_24_rZK5a';
-        if ( file_get_contents( $url ) ) {
-          $data = mb_convert_encoding( file_get_contents( $url ), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
-          $json_decode = json_decode($data, true);
-          return $json_decode;
-        }
-      }
-      function get_food_data() {
-        $url = 'https://script.googleusercontent.com/macros/echo?user_content_key=8M8cDXXeyVP4Ev8AOmT8RxxflHoutkTnir7BbxXwmRSEwqugmkC8Y3uNVpHwpBKypPcX9WcgaytU9pBUDUNktPGuySMm_Wh-m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMsOL5C9BpDRY-M5mpxKexHDmfg0sH4SCHEn1jSmdH6i7YzXD3W0EuowoYmGzIeMlUghh3CC7hlAyedGI1bBBFuroEXrdCm8Pw&lib=MHT1O2dQR523fwwczY4y04By_24_rZK5a';
-        if ( file_get_contents( $url ) ) {
-          $data = mb_convert_encoding( file_get_contents( $url ), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
-          $json_decode = json_decode($data, true);
-          return $json_decode;
-        }
-      }
-      $lucks  = get_luck_data();
-      $items  = get_item_data();
-      $spots  = get_spot_data();
-      $foods  = get_food_data();
-      $randluck = rand(0,8);
-      $lucktitle = $lucks[$randluck]["title"];
-      $luckcontents = $lucks[$randluck]["content"];
+      // function get_luck_data() {
+      //   $url = 'https://script.googleusercontent.com/macros/echo?user_content_key=IFb5O_hshyKz-lwgO-9PXyIB-vr6qv58ZXm7oE4DBsQpSL8VQLqjpBnF1MyT9rLjj0FkfRAhRomEIXpFWwj2-9O4e2zoBU6cm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPjA4vZP3kadAuJuzpzL5koPUII50xE12s06RXRup_1s2P2olDgsFnQCfDGcbcVYl0tUWAzVnsI5cSoiKtjBZBtY_lYMCeX2Q9z9Jw9Md8uu&lib=MGCGSwU88mgjR4EiuwwnVWK_uYdcHmPPI';
+      //   if ( file_get_contents( $url ) ) {
+      //     $data = mb_convert_encoding( file_get_contents( $url ), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
+      //     $json_decode = json_decode($data, true);
+      //     return $json_decode;
+      //   }
+      // }
+      // function get_item_data() {
+      //   $url = 'https://script.googleusercontent.com/macros/echo?user_content_key=5ISYrODQUHB60gPs-ZkMWNgUEe-r6MygDcTdoNtlT92q-jSH3JaiVR9XrGZ_sMnb4tnEggmJ-z5d2ml3Nl7vuEcGyAm49CNXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnAUP0sBXi52xIy_p6v-6Lz4pj5WAk1clfHKWY_g6Usl8B3I9LSX7b9wbimy255tdC47ZUUuF93hWMwwv3CkrwcS7KsGIIxohiw&lib=MURsQyvxp9xRfkImfm-2mbhy_24_rZK5a';
+      //   if ( file_get_contents( $url ) ) {
+      //     $data = mb_convert_encoding( file_get_contents( $url ), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
+      //     $json_decode = json_decode($data, true);
+      //     return $json_decode;
+      //   }
+      // }
+      // function get_spot_data() {
+      //   $url = 'https://script.googleusercontent.com/macros/echo?user_content_key=cDVjNLGlSB51MLhFkDr1N38k-1rToEJP400Vd-WR8MOD9WEp_8gTEJSKaQET1h3nNu8BMNDD315d2ml3Nl7vuKXZzMK4L4R1m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnGrd7XCOjJun8jMlrgZtXTEN5zJblhTiqjkYYVAok-k5cfhfMQLJ_aonOidiBM2f90Es2jQ7FfD6RXV4URG5OlcRBhJtVIztNA&lib=MRM0HZ0153TD_AdZUnIivUxy_24_rZK5a';
+      //   if ( file_get_contents( $url ) ) {
+      //     $data = mb_convert_encoding( file_get_contents( $url ), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
+      //     $json_decode = json_decode($data, true);
+      //     return $json_decode;
+      //   }
+      // }
+      // function get_food_data() {
+      //   $url = 'https://script.googleusercontent.com/macros/echo?user_content_key=8M8cDXXeyVP4Ev8AOmT8RxxflHoutkTnir7BbxXwmRSEwqugmkC8Y3uNVpHwpBKypPcX9WcgaytU9pBUDUNktPGuySMm_Wh-m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMsOL5C9BpDRY-M5mpxKexHDmfg0sH4SCHEn1jSmdH6i7YzXD3W0EuowoYmGzIeMlUghh3CC7hlAyedGI1bBBFuroEXrdCm8Pw&lib=MHT1O2dQR523fwwczY4y04By_24_rZK5a';
+      //   if ( file_get_contents( $url ) ) {
+      //     $data = mb_convert_encoding( file_get_contents( $url ), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN' );
+      //     $json_decode = json_decode($data, true);
+      //     return $json_decode;
+      //   }
+      // }
+      // $lucks  = get_luck_data();
+      // $items  = get_item_data();
+      // $spots  = get_spot_data();
+      // $foods  = get_food_data();
+      $luckjson = file_get_contents("luck.json");
+      $luckjson = mb_convert_encoding($luckjson, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+      $luckjson = trim($luckjson);
+      $lucks = json_decode($luckjson,true);
+
+      $cntlucks = count($lucks) - 1;
+      $randluck = rand(0,$cntlucks);
+      $lucktitle = $lucks[$randluck]["TITLE"];
+      $luckcontents = $lucks[$randluck]["CONTENT"];
       $luckcontent = explode("。",$luckcontents);
+
+      $itemjson = file_get_contents("item.json");
+      $itemjson = mb_convert_encoding($itemjson, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+      $itemjson = trim($itemjson);
+      $items = json_decode($itemjson,true);
+      $cntitems = count($items) - 1;
+      $randitem = rand(0,$cntitems);
+      $itemtitle = $items[$randitem]["TITLE"];
+      $itemcontents = $items[$randitem]["CONTENT"];
+      $itemcontent = explode("。",$itemcontents);
+
+      $spotjson = file_get_contents("spot.json");
+      $spotjson = mb_convert_encoding($spotjson, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+      $spotjson = trim($spotjson);
+      $spots = json_decode($spotjson,true);
+      $cntspots = count($spots) - 1;
+      $randspot = rand(0,$cntspots);
+      $spottitle = $spots[$randspot]["TITLE"];
+      $spotcontents = $spots[$randspot]["CONTENT"];
+      $spotcontent = explode("。",$spotcontents);
+
+      $foodjson = file_get_contents("food.json");
+      $foodjson = mb_convert_encoding($foodjson, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+      $foodjson = trim($foodjson);
+      $foods = json_decode($foodjson,true);
+      $cntfoods = count($foods) - 1;
+      $randfood = rand(0,$cntfoods);
+      $foodtitle = $foods[$randfood]["TITLE"];
+      $foodcontents = $foods[$randfood]["CONTENT"];
+      $foodcontent = explode("。",$foodcontents);
+
       $randall = rand(0,2);
       if ($randall == 0) {
         $bottomh = 'ラッキーアイテム';
-        $randitem = rand(0,1);
-        $bottomtitle = $items[$randitem]["title"];
-        $itemcontents = $items[$randitem]["content"];
+        $randitem = rand(0,$cntitems);
+        $bottomtitle = $items[$randitem]["TITLE"];
+        $itemcontents = $items[$randitem]["CONTENT"];
         $bottomcontent = explode("。",$itemcontents);
       } else if ($randall == 1) {
         $bottomh = 'ラッキースポット';
-        $randspot = rand(0,1);
-        $bottomtitle = $spots[$randspot]["title"];
-        $spotcontents = $spots[$randspot]["content"];
+        $randspot = rand(0,$cntspots);
+        $bottomtitle = $spots[$randspot]["TITLE"];
+        $spotcontents = $spots[$randspot]["CONTENT"];
         $bottomcontent = explode("。",$spotcontents);
       } else {
         $bottomh = 'ラッキーフード';
-        $randfood = rand(0,1);
-        $bottomtitle = $foods[$randfood]["title"];
-        $foodcontents = $foods[$randfood]["content"];
+        $randfood = rand(0,$cntfoods);
+        $bottomtitle = $foods[$randfood]["TITLE"];
+        $foodcontents = $foods[$randfood]["CONTENT"];
         $bottomcontent = explode("。",$foodcontents);
       }
       ?>
