3d3a609ea68cf28faa69a2231665cc59630dd6e6
SSM <cvwwvc102@gmail.com>
2022-09-22 19:01:31 +0900

    動画非表示

--- main.php
+++ main.php
@@ -89,7 +89,7 @@
 	<div id="movie">
 		<div id="iframeoverlay">
 			<!-- <iframe width="560" height="315" src="#" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-			<iframe src="https://player.vimeo.com/video/509399644?h=e8137c8b95"frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+			<!-- <iframe src="#"frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe> -->
 		</div>
 	</div>
 	<div id="brochure">
